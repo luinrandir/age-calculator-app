aa6c195d006e7cb81c276d4c61390270aac3a7fb
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2023-04-27 00:19:32 -0700

    Finished test suite, working on function.

--- age.js
+++ age.js
@@ -2,12 +2,20 @@ const year = document.querySelector("[data-number='year']");
 const months = document.querySelector("[data-number='months']");
 const days = document.querySelector("[data-number='days']");
 
-function calculateAge(year, month, day) {
-  let timePassed = {};
-  timePassed.years = year;
-  timePassed.months = month;
-  timePassed.days = day;
-  return timePassed;
+function calculateAge(year, month, day, date = new Date()) {
+  let currentAge = {};
+  currentAge.years = date.getFullYear() - year;
+  currentAge.months = date.getMonth() + 1 - month;
+  currentAge.days = date.getDate() - day;
+  if (currentAge.months < 0) {
+    currentAge.years = currentAge.years - 1;
+    currentAge.months = 12 + currentAge.months;
+  }
+  if (currentAge.days < 0) {
+    currentAge.months = currentAge.months - 1;
+    currentAge.days = 30 + currentAge.days;
+  }
+  return currentAge;
 }
 
 module.exports = calculateAge;
--- age.test.js
+++ age.test.js
@@ -1,5 +1,32 @@
 const calculateAge = require("./age");
 
-test("Quick check to add year, month, date as a + b + c ->", () => {
-  expect(calculateAge(1, 2, 3)).toEqual({ years: 1, months: 2, days: 3 });
-});
+const expectedResults = [
+  { years: 32, months: 3, days: 22 },
+  { years: 32, months: 0, days: 10 },
+  { years: 68, months: 7, days: 12 },
+  { years: 18, months: 1, days: 10 },
+  { years: 33, months: 0, days: 13 },
+  { years: 23, months: 0, days: 18 },
+  { years: 24, months: 0, days: 18 },
+  { years: 13, months: 1, days: 26 },
+  { years: 3, months: 3, days: 18 },
+  { years: 1, months: 5, days: 1 },
+];
+const testInputs = [
+  [1990, 1, 3, new Date("4-25-2022")],
+  [1975, 12, 23, new Date("1-2-2008")],
+  [1954, 5, 13, new Date("12-25-2022")],
+  [2000, 8, 22, new Date("10-2-2018")],
+  [1977, 9, 5, new Date("9-18-2010")],
+  [1999, 2, 24, new Date("3-14-2022")],
+  [1998, 2, 24, new Date("3-14-2022")],
+  [1989, 6, 30, new Date("8-25-2002")],
+  [2010, 2, 3, new Date("5-21-2013")],
+  [2004, 4, 15, new Date("9-16-2005")],
+];
+
+for (let i in testInputs) {
+  test(`Test #${parseInt(i) + 1}`, () => {
+    expect(calculateAge(...testInputs[i])).toEqual(expectedResults[i]);
+  });
+}
